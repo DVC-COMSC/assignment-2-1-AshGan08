269b6655309ab23a66a5ed1a778dfa0424e113c2
AshGan08 <89492623+AshGan08@users.noreply.github.com>
2023-06-21 19:51:13 +0000

    Outputs resulting properly

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def main():
 
     total = num_male + num_fem
 
-    m_perc = (num_male / (num_fem + num_male)) *100 
-    f_perc = (num_fem / (num_fem + num_male)) *100
+    m_perc = float((num_male / (num_fem + num_male)) *100)
+    f_perc = float((num_fem / (num_fem + num_male)) *100)
 
     return total, num_male, num_fem, m_perc, f_perc
 
