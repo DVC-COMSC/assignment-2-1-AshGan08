e072dc792368b9396e86bf49e49ede7457066173
AshGan08 <89492623+AshGan08@users.noreply.github.com>
2023-06-21 19:45:14 +0000

    Program = finished, expected output = same as order on assignment slides

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
+    num_male = int(input("# of male students: "))
+    num_fem = int(input("# of female students: "))
 
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
-    return m_perc, f_perc
+    total = num_male + num_fem
+
+    m_perc = (num_male / (num_fem + num_male)) *100 
+    f_perc = (num_fem / (num_fem + num_male)) *100
+
+    return total, num_male, num_fem, m_perc, f_perc
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
