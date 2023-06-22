88d05d53fe90967254c2bd1bd2bf98b15698a1d0
AshGan08 <agandluru897@insite.4cd.edu>
2023-06-22 12:00:48 -0700

    Finished final version...

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ def main():
     num_male = int(input("# of male students: "))
     num_fem = int(input("# of female students: "))
 
-    total = num_male + num_fem
-
     m_perc = float((num_male / (num_fem + num_male)) *100)
     f_perc = float((num_fem / (num_fem + num_male)) *100)
 
-    return total, num_male, num_fem, m_perc, f_perc
-
+    return m_perc, f_perc
 
 if __name__ == '__main__':
-    total, num_male, num_fem, m_perc, f_perc = main() 
-    print (f'Calculating Total students within classroom \t {total: .2f}')
-    print (f'Calculating male students in class \t {num_male: .2f}')
-    print (f'Calculating female students in class \t {num_fem: .2f}')
+    m_perc, f_perc = main() 
     print (f'Calculating female percentages within classroom \t {f_perc: .2f}')
     print (f'Calculating male percentages within classroom \t {m_perc: .2f}')
